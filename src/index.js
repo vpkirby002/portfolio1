0f02afafce0f921cc9baeee56cc06b1b76b3d1c6
vpkirby002 <vpkirby002@gmail.com>
2017-12-14 00:15:14 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './theme/globalStyle';
+
+ReactDOM.render(
+	routes,
+	document.getElementById('root')
+	);
+
+
+
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
---
+++ src/theme/globalStyle.js
@@ -0,0 +1,17 @@
+import { injectGlobal } from 'styled-components';
+
+/* eslint-disable */
+injectGlobal`
+	@import url('https://fonts.googleapis.com/css?family=VT323');
+
+	body {
+		font-family: 'VT323', monospace;
+		font-size: 25px;
+		}
+
+		h1 {
+			font-size: 60px;
+			text-align: center;
+		}
+
+`
